8578de04163d6c6df4ee0732bb50aac2da26ccf6
Daryl Collins <daryl@litprotocol.com>
2025-10-23 22:22:47 +0100

    chore: Add `changeJobsInterval` binary to easy modification of existing job run intervals

---
+++ packages/yield-backend/src/bin/changeJobsInterval.ts
@@ -0,0 +1,56 @@
+import yargs from 'yargs';
+
+/* eslint-disable no-console */
+import { createAgenda } from '../lib/agenda/agendaClient';
+import { JobType } from '../lib/jobs/optimizeYield';
+
+async function main() {
+  const { commit, interval } = yargs(process.argv.slice(2))
+    .options({
+      commit: {
+        default: false,
+        description:
+          'Commit changes to the database (default does not save any changes, just prints job counts and validates args)',
+        type: 'boolean',
+      },
+      interval: {
+        choices: ['daily', 'weekly', 'monthly'],
+        description: 'How often all jobs should be run.',
+        required: true,
+        type: 'string',
+      },
+    })
+    .version()
+    .help()
+    .parseSync();
+
+  const agenda = await createAgenda();
+
+  // This script modifies _all_ jobs to the provided interval, indescriminately
+  // @ts-expect-error ASSUMPTION: All jobs in the DB are of the same type
+  const jobs: JobType[] = await agenda.jobs();
+
+  const totalJobs = jobs.length;
+  let currentJob = 1;
+  for (const job of jobs) {
+    console.log(
+      `Updating job for ${job.attrs.data.pkpInfo.ethAddress} to repeat ${interval} (${currentJob}/${totalJobs})`
+    );
+
+    job.repeatEvery(interval);
+
+    if (commit) {
+      // eslint-disable-next-line no-await-in-loop
+      await job.save();
+    }
+
+    currentJob += 1;
+  }
+
+  // Shut down agenda connections and let the process exit gracefully
+  await agenda.stop();
+
+  process.exit(0);
+}
+
+main();
